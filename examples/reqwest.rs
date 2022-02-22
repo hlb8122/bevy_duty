4c5a7a2de58774c0ee9ce7874b65e041fe9d95c9
Harry Barber <harrybarber@protonmail.com>
2022-02-22 00:09:46 +0000

    First attempt

---
+++ examples/reqwest.rs
@@ -0,0 +1,38 @@
+use async_compat::Compat;
+use bevy::{log::LogPlugin, prelude::*};
+use bevy_duty::{task, ExecutorPlugin, Output};
+use reqwest::Error;
+
+fn startup(mut commands: Commands) {
+    // Construct a Future
+    let fut = async {
+        let response = reqwest::get("https://jsonplaceholder.typicode.com/todos/1").await?;
+        response.text().await
+    };
+
+    // Turn it into a Bundle
+    let task = task(Compat::new(fut));
+
+    // Insert the Bundle
+    commands.spawn().insert_bundle(task);
+}
+
+type JSONOutput = Output<Result<String, Error>>;
+
+fn print_result(query: Query<&JSONOutput, Added<JSONOutput>>) {
+    if let Ok(result) = query.get_single() {
+        let text = result.0.as_ref().expect("failed to load");
+        info!(message = "found result", %text);
+        panic!("hey");
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(MinimalPlugins)
+        .add_plugin(LogPlugin)
+        .add_plugin(ExecutorPlugin)
+        .add_startup_system(startup)
+        .add_system(print_result)
+        .run();
+}
---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use bevy::{ecs::system::EntityCommands, prelude::*};
+use crossbeam_channel::{unbounded, Receiver, Sender};
+use waker_fn::waker_fn;
+
+#[derive(Component)]
+pub struct Awake;
+
+#[derive(Component)]
+pub struct Output<T>(pub T);
+
+type BoxEntityCommands = Box<dyn FnOnce(&mut EntityCommands) + 'static>;
+
+/// Represents an async task. Spawning this
+#[derive(Component)]
+struct Task(Pin<Box<dyn Future<Output = BoxEntityCommands> + Send + Sync + 'static>>);
+
+/// Constructs a [`Bundle`] which, when inserted into an entity, will cause an [`Output`] to be
+/// added when the [`Future`] resolves.
+pub fn task<F>(fut: F) -> impl Bundle
+where
+    F: Future + Send + Sync + 'static,
+    F::Output: Send + Sync + 'static,
+{
+    let fut = async move {
+        let output = fut.await;
+
+        let f = Box::new(move |entity_commands: &mut EntityCommands| {
+            entity_commands.insert(Output(output));
+        }) as Box<dyn FnOnce(&mut EntityCommands)>;
+        f
+    };
+    (Task(Box::pin(fut)), Awake)
+}
+
+struct Executor {
+    sender: Sender<Entity>,
+    receiver: Receiver<Entity>,
+}
+
+fn wake_system(res: Res<Executor>, mut commands: Commands) {
+    for id in res.receiver.try_iter() {
+        trace!(message = "waking entity", ?id);
+        commands.entity(id).insert(Awake);
+    }
+}
+
+fn poll_system(
+    res: ResMut<Executor>,
+    mut query: Query<(Entity, &mut Task), With<Awake>>,
+    mut commands: Commands,
+) {
+    for (id, mut task) in query.iter_mut() {
+        let sender = res.sender.clone();
+        let waker = waker_fn(move || {
+            sender.send(id).expect("executor dropped");
+        });
+        let mut cx = Context::from_waker(&waker);
+        let result = task.0.as_mut().poll(&mut cx);
+
+        let mut entity_commands = commands.entity(id);
+        entity_commands.remove::<Awake>();
+        match result {
+            Poll::Ready(output) => output(&mut entity_commands),
+            Poll::Pending => (),
+        }
+    }
+}
+
+pub struct ExecutorPlugin;
+
+impl Plugin for ExecutorPlugin {
+    fn build(&self, app: &mut App) {
+        let (sender, receiver) = unbounded();
+        let executor = Executor { sender, receiver };
+        app.insert_resource(executor)
+            .add_system(wake_system)
+            .add_system(poll_system);
+    }
+}
