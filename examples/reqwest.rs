f23bec7a463b47aaf5109e0a7eae754b8f8ab1c0
Harry Barber <harrybarber@protonmail.com>
2022-02-22 00:32:31 +0000

    Channel spin

--- examples/reqwest.rs
+++ examples/reqwest.rs
@@ -1,29 +1,35 @@
 use async_compat::Compat;
-use bevy::{log::LogPlugin, prelude::*};
-use bevy_duty::{task, ExecutorPlugin, Output};
+use bevy::{log::LogPlugin, prelude::*, tasks::IoTaskPool};
+use crossbeam_channel::{bounded, Receiver};
 use reqwest::Error;
 
-fn startup(mut commands: Commands) {
+#[derive(Component)]
+pub struct Output<T>(pub Receiver<T>);
+
+fn startup(pool: Res<IoTaskPool>, mut commands: Commands) {
+    let (sender, receiver) = bounded(1);
+
     // Construct a Future
-    let fut = async {
+    let fut = async move {
         let response = reqwest::get("https://jsonplaceholder.typicode.com/todos/1").await?;
         response.text().await
     };
+    let fut = async move {
+        sender.send(fut.await).unwrap();
+    };
 
-    // Turn it into a Bundle
-    let task = task(Compat::new(fut));
+    let task = pool.spawn(Compat::new(fut));
 
-    // Insert the Bundle
-    commands.spawn().insert_bundle(task);
+    commands.spawn().insert(Output(receiver)).insert(task);
 }
 
 type JSONOutput = Output<Result<String, Error>>;
 
-fn print_result(query: Query<&JSONOutput, Added<JSONOutput>>) {
-    if let Ok(result) = query.get_single() {
-        let text = result.0.as_ref().expect("failed to load");
+fn print_result(mut query: Query<&mut JSONOutput>) {
+    let output = query.single_mut();
+    if let Ok(result) = output.0.try_recv() {
+        let text = result.as_ref().expect("failed to load");
         info!(message = "found result", %text);
-        panic!("hey");
     }
 }
 
@@ -31,7 +37,7 @@ fn main() {
     App::new()
         .add_plugins(MinimalPlugins)
         .add_plugin(LogPlugin)
-        .add_plugin(ExecutorPlugin)
+        // .add_plugin(ExecutorPlugin)
         .add_startup_system(startup)
         .add_system(print_result)
         .run();
